6b330b47e6e4a652f80acf8f501cc67e715ec547
Will Beaumont <39424606+nmbeau@users.noreply.github.com>
2023-01-16 13:32:10 -0800

    update tailwind config for third color

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
       colors: {
         "bg-pri": "#4ade80",
         "bg-sec": "#15803d",
+        "bg-tri": "#10b981",
         "txt-pri": "#14532d",
         "txt-sec": "#f0fdf4",
       },
